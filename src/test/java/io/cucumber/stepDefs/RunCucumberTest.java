4b2db92b46909ac96a6fdd894cb7aeef352aaf34
HusseinNourAlhuda <husseinnour00@gmail.com>
2022-11-21 11:14:13 +0100

    POM-Plugin-Surefire

--- src/test/java/io/cucumber/stepDefs/RunCucumberTest.java
+++ src/test/java/io/cucumber/stepDefs/RunCucumberTest.java
@@ -13,6 +13,5 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("io/cucumber/features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io/cucumber/stepDefs")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/report/report.html")
 public class  RunCucumberTest {
 }
